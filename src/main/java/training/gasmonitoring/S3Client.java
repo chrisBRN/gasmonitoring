4c2080a3f8b62c941433c10be2abd402853e319c
Chris <chriskbrown@hotmail.co.uk>
2019-12-12 15:59:12 +0000

    S3Client Refactor

--- src/main/java/training/gasmonitoring/S3Client.java
+++ src/main/java/training/gasmonitoring/S3Client.java
@@ -1,6 +1,5 @@
 package training.gasmonitoring;
 
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
@@ -8,46 +7,16 @@ import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import java.io.IOException;
-import java.util.ArrayList;
+
 import java.util.List;
 
 public class S3Client {
 
-    private final static Logger logger = LogManager.getLogger();
-
-    static List<Location> fetchJsonFileFromBucket(String bucket_name, String key_name, Regions regions) {
-
-        final String generalLog = "Downloading " + key_name + " from S3 bucket " + bucket_name;
-
-        logger.info(generalLog + " ...");
-        List<Location> locations = new ArrayList<>();
+    static List<Location> fetchJsonAsListFromBucket(String bucket_name, String key_name, Regions regions) throws Exception {
         final AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard().withRegion(regions).build();
-
-        try {
-            S3Object s3Object = amazonS3.getObject(bucket_name, key_name);
-            S3ObjectInputStream s3ObjectInputStream = s3Object.getObjectContent();
-            ObjectMapper objectMapper = new ObjectMapper();
-
-            locations = objectMapper.readValue(s3ObjectInputStream, new TypeReference<List<Location>>(){});
-
-        } catch (AmazonServiceException e) {
-            logger.error(e.getErrorMessage());
-            logger.info(generalLog + " FAILURE");
-
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-            logger.info(generalLog + " FAILURE");
-        }
-
-        if (locations == null) {
-            logger.info(generalLog + " FAILURE");
-        } else {
-            logger.info(generalLog + " SUCCESS");
-        }
-
-        return locations;
+        S3Object s3Object = amazonS3.getObject(bucket_name, key_name);
+        S3ObjectInputStream s3ObjectInputStream = s3Object.getObjectContent();
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(s3ObjectInputStream, new TypeReference<List<Location>>(){});
     }
 }
